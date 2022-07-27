b04ef302433c7bf88842e48b4231b6d3afff73e7
kiarash.borooshan <kiarash.borooshan@gmail.com>
2022-07-27 17:37:54 +0430

    Admin decorate and requirement.txt

--- MyWeb/Bestoon/admin.py
+++ MyWeb/Bestoon/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Expense, Income
-
 # Register your models here.
 
-admin.site.register(Expense)
-admin.site.register(Income)
+# admin.site.register(Expense)
+# admin.site.register(Income)
+
+
+@admin.register(Expense)
+class Expense_Admin(admin.ModelAdmin):
+    list_display = ("Choose", "Amount")
+
+
+@admin.register(Income)
+class Income_Admin(admin.ModelAdmin):
+    list_display = ("Amount", "User")
