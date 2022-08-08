913b4540db7dffff046a0492bd0e1d54c54021e8
kiarash.borooshan <kiarash.borooshan@gmail.com>
2022-08-08 10:17:11 +0430

    food app created and food model added

---
+++ MyWeb/foods/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class food(models.Model):
+    Name = models.CharField(max_length=100,
+                            verbose_name="نام غذا")
+    Description = models.CharField(max_length=50,
+                                   verbose_name="توضیحات")
+    Rate = models.IntegerField(verbose_name="امتیاز")
+    Price = models.BigIntegerField(verbose_name="قیمت")
+    Time = models.IntegerField(verbose_name="زمان لازم")
+    Pub_date = models.DateTimeField(auto_now=False,
+                                    auto_now_add=True,
+                                    verbose_name="تاریخ")
+    Photo = models.ImageField(upload_to="foods/image/",
+                              verbose_name="تصویر")
